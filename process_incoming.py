740de2fbab5710d1001aa9fcdcbd2e417b708656
Kabir Mehta <159567684+mehtakabir@users.noreply.github.com>
2026-01-03 20:21:46 +0530

    Update process_incoming.py

--- process_incoming.py
+++ process_incoming.py
@@ -10,7 +10,6 @@ client=OpenAI(api_key=api_key)
 
 
 def create_embedding(text_list):
-    # https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings
     r = requests.post("http://localhost:11434/api/embed", json={
         "model": "bge-m3",
         "input": text_list
@@ -18,19 +17,7 @@ def create_embedding(text_list):
 
     embedding = r.json()["embeddings"] 
     return embedding
-
-# def inference(prompt):
-#     r = requests.post("http://localhost:11434/api/generate", json={
-#         # "model": "deepseek-r1",
-#         "model": "llama3.2",
-#         "prompt": prompt,
-#         "stream": False
-#     })
-
-#     response = r.json()
-#     print(response)
-#     return response
-
+    
 def inference_openai(prompt):
     print("Thinking...")
     response=client.responses.create(
@@ -46,16 +33,13 @@ df = joblib.load('embeddings.joblib')
 incoming_query = input("Ask a Question: ")
 question_embedding = create_embedding([incoming_query])[0] 
 
-# Find similarities of question_embedding with other embeddings
-# print(np.vstack(df['embedding'].values))
-# print(np.vstack(df['embedding']).shape)
 similarities = cosine_similarity(np.vstack(df['embedding']), [question_embedding]).flatten()
-# print(similarities)
+
 top_results = 5
 max_indx = similarities.argsort()[::-1][0:top_results]
-# print(max_indx)
+
 new_df = df.loc[max_indx] 
-# print(new_df[["title", "number", "text"]])
+
 
 prompt = f'''I am teaching Machine Learning Course. Here are video subtitle chunks containing video title, video number, start time in seconds, end time in seconds, the text at that time:
 
@@ -67,12 +51,8 @@ User asked this question related to the video chunks, you have to answer in a hu
 with open("prompt.txt", "w") as f:
     f.write(prompt)
 
-# response = inference(prompt)["response"]
-# print(response)
 
 response=inference_openai(prompt)
 
 with open("response.txt", "w", encoding="utf-8") as f:
     f.write(response)
-# for index, item in new_df.iterrows():
-#     print(index, item["title"], item["number"], item["text"], item["start"], item["end"])
